7a57bc2a5f6f00399d4b3fe4e2b1a2a86229acc6
Sayon <76528979+xaidenwave@users.noreply.github.com>
2022-01-08 14:46:25 +0530

    Dwmblocks with clock and volume.

---
+++ .local/src/dwmblocks/blocks.def.h
@@ -0,0 +1,8 @@
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
+	{"",	"volume",	0,	10},
+	{"",	"clock",	60,	1}
+};
+
+static char delim[] = " / ";
+static unsigned int delimLen = 5;
