8a28909e5ed4a0f0bea4160494fda247e2c2e03b
Sayon <76528979+xaidenwave@users.noreply.github.com>
2022-01-08 16:44:58 +0530

    Update config.h

--- .local/src/st-0.8.4/config.h
+++ .local/src/st-0.8.4/config.h
@@ -10,8 +10,8 @@ static char *font = "Cascadia Code:pixelsize=12:antialias=true:autohint=true";
 static char *font2[] = {
 	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
 	"Hack Nerd Font Mono:pixelsize=12:antialias=true:autohint=true",
-	"Noto Color Emoji:pixelsize=14:antialias=true:autohint=true",
-	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
+	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 
 static int borderpx = 2;
