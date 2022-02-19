140cfabd061f8d3da876955f149ce361473a7664
Sayon <76528979+xaidenwave@users.noreply.github.com>
2022-02-19 13:34:29 +0530

    Dmenu with emoji support

--- .local/src/dmenu-5.0/config.h
+++ .local/src/dmenu-5.0/config.h
@@ -5,8 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Fira Code:size=10",
-	"Noto Color Emoji:pixelsize=15",
-	"JoyPixels:pixelsize=12"
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
